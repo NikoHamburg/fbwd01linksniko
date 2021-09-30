90de3c4f0e75adc254ff3af649f93f9d48aeefd0
NikoHamburg <nikotrettin@googlemail.com>
2021-09-30 13:58:39 +0200

    add stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import links from "./data/links.json";
 
-function App() {
+const App = () => {
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/NikoHamburg/fbwd01linksniko/main/src/data/links.json"
+      );
+      const data = await response.json();
+      setLinks(data);
+    })();
+  }, []);
   return (
     <div className="App">
       <h1>Links</h1>
@@ -18,6 +29,6 @@ function App() {
       </ul>
     </div>
   );
-}
+};
 
 export default App;
